b0f781512a34cecb9c81652f71aeee84d4ad1249
FelipeKogake <felipe.kogake77@gmail.com>
2026-02-06 12:14:26 -0300

    Adding toString into model class Student

--- src/main/java/com/example/koribackend/model/entity/Student.java
+++ src/main/java/com/example/koribackend/model/entity/Student.java
@@ -5,15 +5,15 @@ import java.sql.Date;
 public class Student {
     private int enrollment;
     private String email;
-    private Date issue_date;
+    private Date issueDate;
     private String password;
     private String name;
     private int serie;
 
-    public Student(int enrollment, String email, Date issue_date, String password, String name, int serie) {
+    public Student(int enrollment, String email, Date issueDate, String password, String name, int serie) {
         this.enrollment = enrollment;
         this.email = email;
-        this.issue_date = issue_date;
+        this.issueDate = issueDate;
         this.password = password;
         this.name = name;
         this.serie = serie;
@@ -37,12 +37,12 @@ public class Student {
         this.email = email;
     }
 
-    public Date getIssue_date() {
-        return issue_date;
+    public Date getIssueDate() {
+        return issueDate;
     }
 
-    public void setIssue_date(Date issue_date) {
-        this.issue_date = issue_date;
+    public void setIssueDate(Date issueDate) {
+        this.issueDate = issueDate;
     }
 
     public String getPassword() {
@@ -67,5 +67,9 @@ public class Student {
 
     public void setSerie(int serie) {
         this.serie = serie;
+    }
+
+    public String toString() {
+        return "Enrollment: " + this.enrollment + "\nEmail: " + this.email + "Issue date: " + this.issueDate + "Password: " + this.password + "Name: " + this.name + "Serie" + this.serie;
     }
 }
